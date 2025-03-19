9e80640f4549a4c48543f0fa1ebad130ce1bb058
Nelkeys <nelsonekoh212@gmail.com>
2025-03-19 14:37:12 +0100

    reload bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SearchResults from "./pages/SearchResults";
 import Read from "./pages/Read";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<SearchResults />} />
         <Route path="/:title" element={<Read />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
